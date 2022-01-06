778050e7ec59491842ff70a1b3f2aa3dae685c00
Marcos Leite <30819867+mabner@users.noreply.github.com>
2022-01-06 05:36:06 -0300

    db script updates

---
+++ scripts/DB.sql
@@ -0,0 +1,7 @@
+-- Script de criação do DB em andamento
+
+CREATE DATABASE `locafacil`;
+ALTER SCHEMA `locafacil`  DEFAULT CHARACTER SET latin1  DEFAULT COLLATE latin1_swedish_ci;
+
+USE locafacil;
+
--- scripts/estados.sql
+++ scripts/estados.sql
@@ -1,27 +1,31 @@
-INSERT INTO `` (`id_uf`,`uf`,`nome`) VALUES (1,'AC','Acre');
-INSERT INTO `` (`id_uf`,`uf`,`nome`) VALUES (2,'AL','Alagoas');
-INSERT INTO `` (`id_uf`,`uf`,`nome`) VALUES (3,'AP','Amapá');
-INSERT INTO `` (`id_uf`,`uf`,`nome`) VALUES (4,'AM','Amazonas');
-INSERT INTO `` (`id_uf`,`uf`,`nome`) VALUES (5,'BA','Bahia');
-INSERT INTO `` (`id_uf`,`uf`,`nome`) VALUES (6,'CE','Ceara');
-INSERT INTO `` (`id_uf`,`uf`,`nome`) VALUES (7,'DF','Distrito Federal');
-INSERT INTO `` (`id_uf`,`uf`,`nome`) VALUES (8,'ES','Espírito Santo');
-INSERT INTO `` (`id_uf`,`uf`,`nome`) VALUES (9,'GO','Goiás');
-INSERT INTO `` (`id_uf`,`uf`,`nome`) VALUES (10,'MA','Maranhão');
-INSERT INTO `` (`id_uf`,`uf`,`nome`) VALUES (11,'MT','Mato Grosso');
-INSERT INTO `` (`id_uf`,`uf`,`nome`) VALUES (12,'MS','Mato Grosso do Sul');
-INSERT INTO `` (`id_uf`,`uf`,`nome`) VALUES (13,'MG','Minas Gerais');
-INSERT INTO `` (`id_uf`,`uf`,`nome`) VALUES (14,'PA','Pará');
-INSERT INTO `` (`id_uf`,`uf`,`nome`) VALUES (15,'PB','Paraíba');
-INSERT INTO `` (`id_uf`,`uf`,`nome`) VALUES (16,'PR','Paraná');
-INSERT INTO `` (`id_uf`,`uf`,`nome`) VALUES (17,'PE','Pernambuco');
-INSERT INTO `` (`id_uf`,`uf`,`nome`) VALUES (18,'PI','Piauí');
-INSERT INTO `` (`id_uf`,`uf`,`nome`) VALUES (19,'RJ','Rio de Janeiro');
-INSERT INTO `` (`id_uf`,`uf`,`nome`) VALUES (20,'RN','Rio Grande do Norte');
-INSERT INTO `` (`id_uf`,`uf`,`nome`) VALUES (21,'RS','Rio Grande do Sul');
-INSERT INTO `` (`id_uf`,`uf`,`nome`) VALUES (22,'RO','Rondônia');
-INSERT INTO `` (`id_uf`,`uf`,`nome`) VALUES (23,'RR','Roraima');
-INSERT INTO `` (`id_uf`,`uf`,`nome`) VALUES (24,'SC','Santa Catarina');
-INSERT INTO `` (`id_uf`,`uf`,`nome`) VALUES (25,'SP','São Paulo');
-INSERT INTO `` (`id_uf`,`uf`,`nome`) VALUES (26,'SE','Sergipe');
-INSERT INTO `` (`id_uf`,`uf`,`nome`) VALUES (27,'TO','Tocantins');
+/*
+-- Query: 
+-- Date: 2022-01-06 02:46
+*/
+INSERT INTO `` (`id_uf`,`sigla`,`nome`) VALUES (1,'AC','Acre');
+INSERT INTO `` (`id_uf`,`sigla`,`nome`) VALUES (2,'AL','Alagoas');
+INSERT INTO `` (`id_uf`,`sigla`,`nome`) VALUES (3,'AP','Amapá');
+INSERT INTO `` (`id_uf`,`sigla`,`nome`) VALUES (4,'AM','Amazonas');
+INSERT INTO `` (`id_uf`,`sigla`,`nome`) VALUES (5,'BA','Bahia');
+INSERT INTO `` (`id_uf`,`sigla`,`nome`) VALUES (6,'CE','Ceara');
+INSERT INTO `` (`id_uf`,`sigla`,`nome`) VALUES (7,'DF','Distrito Federal');
+INSERT INTO `` (`id_uf`,`sigla`,`nome`) VALUES (8,'ES','Espírito Santo');
+INSERT INTO `` (`id_uf`,`sigla`,`nome`) VALUES (9,'GO','Goiás');
+INSERT INTO `` (`id_uf`,`sigla`,`nome`) VALUES (10,'MA','Maranhão');
+INSERT INTO `` (`id_uf`,`sigla`,`nome`) VALUES (11,'MT','Mato Grosso');
+INSERT INTO `` (`id_uf`,`sigla`,`nome`) VALUES (12,'MS','Mato Grosso do Sul');
+INSERT INTO `` (`id_uf`,`sigla`,`nome`) VALUES (13,'MG','Minas Gerais');
+INSERT INTO `` (`id_uf`,`sigla`,`nome`) VALUES (14,'PA','Pará');
+INSERT INTO `` (`id_uf`,`sigla`,`nome`) VALUES (15,'PB','Paraíba');
+INSERT INTO `` (`id_uf`,`sigla`,`nome`) VALUES (16,'PR','Paraná');
+INSERT INTO `` (`id_uf`,`sigla`,`nome`) VALUES (17,'PE','Pernambuco');
+INSERT INTO `` (`id_uf`,`sigla`,`nome`) VALUES (18,'PI','Piauí');
+INSERT INTO `` (`id_uf`,`sigla`,`nome`) VALUES (19,'RJ','Rio de Janeiro');
+INSERT INTO `` (`id_uf`,`sigla`,`nome`) VALUES (20,'RN','Rio Grande do Norte');
+INSERT INTO `` (`id_uf`,`sigla`,`nome`) VALUES (21,'RS','Rio Grande do Sul');
+INSERT INTO `` (`id_uf`,`sigla`,`nome`) VALUES (22,'RO','Rondônia');
+INSERT INTO `` (`id_uf`,`sigla`,`nome`) VALUES (23,'RR','Roraima');
+INSERT INTO `` (`id_uf`,`sigla`,`nome`) VALUES (24,'SC','Santa Catarina');
+INSERT INTO `` (`id_uf`,`sigla`,`nome`) VALUES (25,'SP','São Paulo');
+INSERT INTO `` (`id_uf`,`sigla`,`nome`) VALUES (26,'SE','Sergipe');
+INSERT INTO `` (`id_uf`,`sigla`,`nome`) VALUES (27,'TO','Tocantins');
