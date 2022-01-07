66d1c53f6c0dc3e970bbcf9e3a757fad37dc0960
Marcos Leite <30819867+mabner@users.noreply.github.com>
2022-01-07 01:32:10 -0300

    Add-Migration InitialCreate

---
+++ LocaFacil/Migrations/20220107042154_InitialCreate.cs
@@ -0,0 +1,275 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LocaFacil.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "latin1");
+
+            migrationBuilder.CreateTable(
+                name: "Telefone",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    ddd = table.Column<string>(type: "varchar(2)", maxLength: 2, nullable: true, collation: "latin1_swedish_ci")
+                        .Annotation("MySql:CharSet", "latin1"),
+                    numero = table.Column<string>(type: "varchar(9)", maxLength: 9, nullable: false, collation: "latin1_swedish_ci")
+                        .Annotation("MySql:CharSet", "latin1")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Telefone", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "latin1")
+                .Annotation("Relational:Collation", "latin1_swedish_ci");
+
+            migrationBuilder.CreateTable(
+                name: "TipoImovel",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Descricao = table.Column<string>(type: "varchar(60)", maxLength: 60, nullable: false, collation: "latin1_swedish_ci")
+                        .Annotation("MySql:CharSet", "latin1")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TipoImovel", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "latin1")
+                .Annotation("Relational:Collation", "latin1_swedish_ci");
+
+            migrationBuilder.CreateTable(
+                name: "Uf",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Sigla = table.Column<string>(type: "varchar(2)", maxLength: 2, nullable: false, collation: "latin1_swedish_ci")
+                        .Annotation("MySql:CharSet", "latin1"),
+                    Nome = table.Column<string>(type: "varchar(20)", maxLength: 20, nullable: false, collation: "latin1_swedish_ci")
+                        .Annotation("MySql:CharSet", "latin1")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Uf", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "latin1")
+                .Annotation("Relational:Collation", "latin1_swedish_ci");
+
+            migrationBuilder.CreateTable(
+                name: "Endereco",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Cep = table.Column<string>(type: "varchar(8)", maxLength: 8, nullable: false, collation: "latin1_swedish_ci")
+                        .Annotation("MySql:CharSet", "latin1"),
+                    Logradouro = table.Column<string>(type: "varchar(60)", maxLength: 60, nullable: false, collation: "latin1_swedish_ci")
+                        .Annotation("MySql:CharSet", "latin1"),
+                    Numero = table.Column<int>(type: "int", maxLength: 12, nullable: false),
+                    Complemento = table.Column<string>(type: "varchar(20)", maxLength: 20, nullable: true, collation: "latin1_swedish_ci")
+                        .Annotation("MySql:CharSet", "latin1"),
+                    Bairro = table.Column<string>(type: "varchar(40)", maxLength: 40, nullable: false, collation: "latin1_swedish_ci")
+                        .Annotation("MySql:CharSet", "latin1"),
+                    Localidade = table.Column<string>(type: "varchar(60)", maxLength: 60, nullable: false, collation: "latin1_swedish_ci")
+                        .Annotation("MySql:CharSet", "latin1"),
+                    UfId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Endereco", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Endereco_Uf_UfId",
+                        column: x => x.UfId,
+                        principalTable: "Uf",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                })
+                .Annotation("MySql:CharSet", "latin1")
+                .Annotation("Relational:Collation", "latin1_swedish_ci");
+
+            migrationBuilder.CreateTable(
+                name: "Locatario",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Nome = table.Column<string>(type: "varchar(60)", maxLength: 60, nullable: false, collation: "latin1_swedish_ci")
+                        .Annotation("MySql:CharSet", "latin1"),
+                    TelefoneId = table.Column<int>(type: "int", nullable: false),
+                    EnderecoId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Locatario", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Locatario_Endereco_EnderecoId",
+                        column: x => x.EnderecoId,
+                        principalTable: "Endereco",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Locatario_Telefone_TelefoneId",
+                        column: x => x.TelefoneId,
+                        principalTable: "Telefone",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                })
+                .Annotation("MySql:CharSet", "latin1")
+                .Annotation("Relational:Collation", "latin1_swedish_ci");
+
+            migrationBuilder.CreateTable(
+                name: "Proprietario",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Nome = table.Column<string>(type: "varchar(60)", maxLength: 60, nullable: false, collation: "latin1_swedish_ci")
+                        .Annotation("MySql:CharSet", "latin1"),
+                    TelefoneId = table.Column<int>(type: "int", nullable: false),
+                    EnderecoId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Proprietario", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Proprietario_Endereco_EnderecoId",
+                        column: x => x.EnderecoId,
+                        principalTable: "Endereco",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Proprietario_Telefone_TelefoneId",
+                        column: x => x.TelefoneId,
+                        principalTable: "Telefone",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                })
+                .Annotation("MySql:CharSet", "latin1")
+                .Annotation("Relational:Collation", "latin1_swedish_ci");
+
+            migrationBuilder.CreateTable(
+                name: "Imovel",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    TipoImovelId = table.Column<int>(type: "int", nullable: false),
+                    EnderecoId = table.Column<int>(type: "int", nullable: false),
+                    ValorAluguel = table.Column<decimal>(type: "decimal(8,2)", nullable: false),
+                    ValorCondominio = table.Column<decimal>(type: "decimal(8,2)", nullable: false),
+                    ValorIPTU = table.Column<decimal>(type: "decimal(8,2)", nullable: false),
+                    VagaGaragem = table.Column<decimal>(type: "decimal(5,0)", nullable: false),
+                    Descricao = table.Column<string>(type: "varchar(400)", maxLength: 400, nullable: false, collation: "latin1_swedish_ci")
+                        .Annotation("MySql:CharSet", "latin1"),
+                    ProprietarioId = table.Column<int>(type: "int", nullable: false),
+                    LocatarioId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Imovel", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Imovel_Endereco_EnderecoId",
+                        column: x => x.EnderecoId,
+                        principalTable: "Endereco",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Imovel_Locatario_LocatarioId",
+                        column: x => x.LocatarioId,
+                        principalTable: "Locatario",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Imovel_Proprietario_ProprietarioId",
+                        column: x => x.ProprietarioId,
+                        principalTable: "Proprietario",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Imovel_TipoImovel_TipoImovelId",
+                        column: x => x.TipoImovelId,
+                        principalTable: "TipoImovel",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                })
+                .Annotation("MySql:CharSet", "latin1")
+                .Annotation("Relational:Collation", "latin1_swedish_ci");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Endereco_UfId",
+                table: "Endereco",
+                column: "UfId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Imovel_EnderecoId",
+                table: "Imovel",
+                column: "EnderecoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Imovel_LocatarioId",
+                table: "Imovel",
+                column: "LocatarioId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Imovel_ProprietarioId",
+                table: "Imovel",
+                column: "ProprietarioId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Imovel_TipoImovelId",
+                table: "Imovel",
+                column: "TipoImovelId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Locatario_EnderecoId",
+                table: "Locatario",
+                column: "EnderecoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Locatario_TelefoneId",
+                table: "Locatario",
+                column: "TelefoneId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Proprietario_EnderecoId",
+                table: "Proprietario",
+                column: "EnderecoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Proprietario_TelefoneId",
+                table: "Proprietario",
+                column: "TelefoneId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Imovel");
+
+            migrationBuilder.DropTable(
+                name: "Locatario");
+
+            migrationBuilder.DropTable(
+                name: "Proprietario");
+
+            migrationBuilder.DropTable(
+                name: "TipoImovel");
+
+            migrationBuilder.DropTable(
+                name: "Endereco");
+
+            migrationBuilder.DropTable(
+                name: "Telefone");
+
+            migrationBuilder.DropTable(
+                name: "Uf");
+        }
+    }
+}
